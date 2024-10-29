2e42be258c0b062da7f6bdbec60372dcab9d1a4e
afk-2 <abdurahman.khan@gmail.com>
2024-10-29 16:30:21 +0400

    create course data in courses.js

---
+++ scripts/courses.js
@@ -0,0 +1,92 @@
+const courses = [
+    {
+        id: '1001',
+        subject: "Art and Craft",
+        location: "Room A1",
+        price: 150,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/arts_and_crafts.jpg"
+    },
+    {
+        id: '1002',
+        subject: "Cooking Basics",
+        location: "Room B2",
+        price: 180,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/cooking_basics.jpg"
+    },
+    {
+        id: '1003',
+        subject: "Science Experiments",
+        location: "Room C3",
+        price: 160,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/science_experiments.jpg"
+    },
+    {
+        id: '1004',
+        subject: "Math Club",
+        location: "Room D4",
+        price: 140,
+        spaces: 5,
+        rating: 3,
+        thumbnail: "images/math_club.jpg"
+    },
+    {
+        id: '1005',
+        subject: "Creative Writing",
+        location: "Room E5",
+        price: 150,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/creative_writing.jpg"
+    },
+    {
+        id: '1006',
+        subject: "Robotics Workshop",
+        location: "Room F6",
+        price: 220,
+        spaces: 5,
+        rating: 5,
+        thumbnail: "images/robotics_workshop.jpg"
+    },
+    {
+        id: '1007',
+        subject: "Photography Basics",
+        location: "Room G7",
+        price: 130,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/photography_basics.jpg"
+    },
+    {
+        id: '1008',
+        subject: "Football Training",
+        location: "Field H8",
+        price: 180,
+        spaces: 5,
+        rating: 5,
+        thumbnail: "images/football_training.jpg"
+    },
+    {
+        id: '1009',
+        subject: "Basketball Skills",
+        location: "Gym I9",
+        price: 175,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/basketball_skills.jpg"
+    },
+    {
+        id: '1010',
+        subject: "Chess Club",
+        location: "Room J10",
+        price: 120,
+        spaces: 5,
+        rating: 4,
+        thumbnail: "images/chess_club.jpg"
+    }
+];
