b5313fd9d82bf1c34b71db6a16b36824cbb10001
afk-2 <abdurahman.khan@gmail.com>
2024-10-29 16:55:30 +0400

    Display course rating in html

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,11 @@ let app = new Vue({
     methods: {
         addToCart(courseId) {
             const course = this.courses.find(course => course.id === courseId);
+
             if (course) {
                 this.cart.push(course);
                 course.spaces--;
             }
-            console.log(this.cart);
         }
     }    
 });
