3f49b73c5a814b1fe944c49c8bbc8de350bb5add
afk-2 <abdurahman.khan@gmail.com>
2024-10-29 16:51:06 +0400

    Add add-to-cart functionality

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,14 @@ let app = new Vue({
         cart: [],
         courses: courses,
     },
+    methods: {
+        addToCart(courseId) {
+            const course = this.courses.find(course => course.id === courseId);
+            if (course) {
+                this.cart.push(course);
+                course.spaces--;
+            }
+            console.log(this.cart);
+        }
+    }    
 });
