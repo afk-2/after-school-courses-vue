5a46d484f236664b226cbd29a857c731f7b59872
afk-2 <abdurahman.khan@gmail.com>
2024-10-31 16:14:33 +0400

    Enhance cart functionality: Check for existing courses, calculate cart quantity, create cart page and toggle between pages

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,36 @@ let app = new Vue({
     el: "#app",
     data: {
         cart: [],
+        cartPage: false,
         courses: courses,
     },
     methods: {
-        addToCart(courseId) {
-            const course = this.courses.find(course => course.id === courseId);
-
+        addToCart: function(courseId) {
+            let course = this.courses.find(course => course.id === courseId);
+        
             if (course) {
-                this.cart.push(course);
+                const newCourse = { ...course, quantity: 1 }; 
+                const existingCourse = this.cart.find(newCourse => newCourse.id === courseId);
+
+                if (existingCourse) {
+                    existingCourse.quantity++;
+                } else {
+                    this.cart.push(newCourse);
+                }
+                
                 course.spaces--;
             }
+        },
+        toggle_page: function() { 
+            this.cartPage = !this.cartPage;
         }
-    }    
+    },
+    computed: {
+        cartItemCount: function() {
+            return this.cart.length;
+        },
+        calculateCartQuantity: function() {
+            return this.cart.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        }
+    } 
 });
